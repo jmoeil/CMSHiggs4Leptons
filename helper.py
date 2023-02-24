4b940e863e4a7b2025f33548479d11232f496e36
jmoeil <juianmoeil2001@gmail.com>
2023-02-24 15:26:49 +0100

    Invariant mass and number of events

--- helper.py
+++ helper.py
@@ -1,4 +1,6 @@
 import ROOT
+import numpy as np
+
 # to be used to avoid display intermediate plotting results
 # ROOT.gROOT.SetBatch(ROOT.kTRUE)
 
@@ -13,11 +15,33 @@ def isgoodlepton(tree, i):
     return True
 
 
-def invariantmass4l(tree, i, j, k, l):
+def invariantmass4l(tree, e, p, mu_minus, mu_plus):
     """
     Computes the invariant mass of four leptons labelled i, j, k, l
     """
-    mass = 0
+    
+    pt = np.array([tree._lPt[e],tree._lPt[p],tree._lPt[mu_minus],tree._lPt[mu_plus]])
+    phi = np.array([tree._lPhi[e],tree._lPhi[p],tree._lPhi[mu_minus],tree._lPhi[mu_plus]])
+    eta = np.array([tree._lEta[e],tree._lEta[p],tree._lEta[mu_minus],tree._lEta[mu_plus]])
+
+    px = pt*np.cos(phi)
+    py = pt*np.sin(phi)
+    pz = pt*np.sinh(eta)
+    
+    p_e = np.array([px[0],py[0],pz[0]])
+    p_p = np.array([px[1],py[1],pz[1]])
+    p_mu_minus = np.array([px[2],py[2],pz[2]])
+    p_mu_plus = np.array([px[3],py[3],pz[3]])
+
+    E_e = np.linalg.norm(p_e)
+    E_p = np.linalg.norm(p_p)
+    E_mu_minus = np.linalg.norm(p_mu_minus)
+    E_mu_plus = np.linalg.norm(p_mu_plus)
+
+    E = E_e+E_p+E_mu_minus+E_mu_plus
+
+    mass = np.sqrt(E**2 - np.linalg.norm(p_e+p_p+p_mu_minus+p_mu_plus)**2)
+    
     return mass
 
 
